fecbdd1c89cc7246435ec43eb520a779102c581d
yifan <dongyifan@outlook.com>
2022-06-25 00:15:11 +0800

    Initial Commit

---
+++ NekoLang/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  NekoLang
+//
+//  Created by YIFAN DONG on 2022/6/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
