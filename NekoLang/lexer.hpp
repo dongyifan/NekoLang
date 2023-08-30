5780deae2f0dc51a48dd11b21c36fcd44c006fd2
DongYifan <dongyifan@percipio.xyz>
2023-08-30 19:04:22 +0800

    fresh restart

--- NekoLang/lexer.hpp
+++ NekoLang/lexer.hpp
@@ -1,36 +1,102 @@
-//
-//  lexer.hpp
-//  NekoLang
-//
-//  Created by YIFAN DONG on 2022/6/25.
-//
-
-#ifndef lexer_hpp
-#define lexer_hpp
+#ifndef neko_lang_hpp_
+#define neko_lang_hpp_
 #include <string>
+#include <string_view>
+#include <stdexcept>
 
-namespace NekoLang {
+namespace nekolan {
 
-struct Token;
+inline constexpr std::string_view ILLEGAL = "ILLEGAL";
+inline constexpr std::string_view EOF_ = "EOF";
+inline constexpr std::string_view IDENT = "IDENT";
+inline constexpr std::string_view INT = "INT";
+inline constexpr std::string_view ASSIGN = "=";
+inline constexpr std::string_view PLUS = "+";
+inline constexpr std::string_view COMMA = ",";
+inline constexpr std::string_view SEMICOLON = ";";
+inline constexpr std::string_view LPAREN = "(";
+inline constexpr std::string_view RPAREN = ")";
+inline constexpr std::string_view LBRACE = "{";
+inline constexpr std::string_view RBRACE = "}";
+inline constexpr std::string_view FUNCTION = "FUNCTION";
+inline constexpr std::string_view LET = "LET";
 
-class Lexer {
-public:
-    Lexer(const std::string& input);
-    
-    Token NextToken();
+
+struct Token
+{
+	std::string_view type;
+	std::string_view literal;
+};
+
+class Lexer
+{
 private:
-    void ReadChar();
-    char PeekChar();
-    std::string ReadIdentifier();
-    std::string ReadNumber();
-    void SkipWhiteSpace();
-    
-    std::string input_;
-    int position_ = 0;
-    int readPosition_ = 0;
-    char ch_ = 0;
+	std::string_view input_;
+	int position_ = 0;
+	int readPosition_ = 0;
+	char ch_ = 0;
+
+public:
+	Lexer(const std::string& input) : input_{input}
+	{
+		read_char();
+	}
+
+	void read_char() 
+	{
+		if (readPosition_ >= input_.size()) {
+			ch_ = 0;
+		}
+		else {
+			ch_ = input_[readPosition_];
+		}
+		position_ = readPosition_;
+		readPosition_ += 1;
+	}
+
+	Token next_token() 
+	{
+		Token token;
+
+		switch (ch_)
+		{
+		case '=':
+			token = { ASSIGN, input_.substr(position_, 1) };
+			break;
+		case ';':
+			token = { SEMICOLON, input_.substr(position_, 1) };
+			break;
+		case '(':
+			token = { LPAREN, input_.substr(position_, 1) };
+			break;
+		case ')':
+			token = { RPAREN, input_.substr(position_, 1) };
+			break;
+		case ',':
+			token = { COMMA, input_.substr(position_, 1) };
+			break;
+		case '+':
+			token = { PLUS, input_.substr(position_, 1) };
+			break;
+		case '{':
+			token = { LBRACE, input_.substr(position_, 1) };
+			break;
+		case '}':
+			token = { RBRACE, input_.substr(position_, 1) };
+			break;
+		case 0:
+			token = { EOF_, "" };
+			break;
+		default:
+			throw std::logic_error{ "next_token shouldn't come to default branch" };
+			break;
+		}
+		read_char();
+		return token;
+	}
 };
 
 }
 
-#endif /* lexer_hpp */
+#endif // !neko_lang_hpp_
+
--- NekoLang/main.cpp
+++ NekoLang/main.cpp
@@ -7,56 +7,72 @@
 
 #include <iostream>
 #include <type_traits>
-#include "token.hpp"
 #include "lexer.hpp"
-#include "repl.hpp"
 
-using namespace NekoLang;
+using namespace nekolan;
+//#include "token.hpp"
+//#include "lexer.hpp"
+//#include "repl.hpp"
+//
+//using namespace NekoLang;
 void TestLexerSimple() {
     Lexer lexer("=+(){},");
     Token token;
     do {
-        token = lexer.NextToken();
-        std::cout << token << "\n";
-    } while (token.type.compare(THEEOF) != 0);
+        token = lexer.next_token();
+        std::cout << token.type << ": " << token.literal << "\n";
+    } while (token.type != EOF_);
 }
-
-void TestLexerWithIdentAndKeyword() {
-    static constexpr const char* code =R"(let five = 5;
-let ten = 10;
-
-let add = func(x, y) {
-    x + y;
-};
-
-let result = add(five, ten);
-!-/*5;
-5 < 10 > 5;
-
-if (5 < 10) {
-    return true;
-} else {
-    return false;
-}
-
-10 == 10;
-10 != 9;
-)";
-    Lexer lexer(code);
-    Token token;
-    do {
-        token = lexer.NextToken();
-        std::cout << token << "\n";
-    } while (token.type.compare(THEEOF) != 0);
-}
+//
+//void TestLexerWithIdentAndKeyword() {
+//    static constexpr const char* code =R"(let five = 5;
+//let ten = 10;
+//
+//let add = func(x, y) {
+//    x + y;
+//};
+//
+//let result = add(five, ten);
+//!-/*5;
+//5 < 10 > 5;
+//
+//if (5 < 10) {
+//    return true;
+//} else {
+//    return false;
+//}
+//
+//10 == 10;
+//10 != 9;
+//)";
+//    Lexer lexer(code);
+//    Token token;
+//    do {
+//        token = lexer.NextToken();
+//        std::cout << token << "\n";
+//    } while (token.type.compare(THEEOF) != 0);
+//}
 
 int main(int argc, const char * argv[]) {
 //    TestLexerWithIdentAndKeyword();
     
-    std::cout << "Hello! This is the Neko programming language!\n";
-    std::cout << "Feel free to type in commands\n";
-    
-    ReplStart(std::cin, std::cout);
+    //std::cout << "Hello! This is the Neko programming language!\n";
+    //std::cout << "Feel free to type in commands\n";
+    //
+    //ReplStart(std::cin, std::cout);
+
+    //std::string input = "=+(){},;";
+    //Lexer lexer{ input };
+    //
+    //for (Token token = lexer.next_token(); token.type != EOF_; token = lexer.next_token())
+    //{
+    //    std::cout << token.type << ": " << token.literal << '\n';
+    //}
+
+    TestLexerSimple();
+
+
+    std::cout << "Hello World!";
     
     return 0;
 }
