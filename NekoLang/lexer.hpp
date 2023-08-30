41a922041e11744a21748f855ce56341a30db6d8
yifan <dongyifan@outlook.com>
2023-08-31 00:48:32 +0800

    new lexer

--- NekoLang/lexer.hpp
+++ NekoLang/lexer.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <string_view>
 #include <stdexcept>
+#include <map>
 
 namespace nekolan {
 
@@ -22,78 +23,111 @@ inline constexpr std::string_view FUNCTION = "FUNCTION";
 inline constexpr std::string_view LET = "LET";
 
 
+bool is_letter(char ch)
+{
+    return ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z') || ch == '_';
+}
+
+using TokenType = std::string_view;
+
 struct Token
 {
-	std::string_view type;
-	std::string_view literal;
+    std::string_view type;
+    std::string_view literal;
 };
 
 class Lexer
 {
+    inline static std::map<std::string_view, TokenType> keywords = {
+        {"fn", FUNCTION},
+        {"let", LET}
+    };
 private:
-	std::string_view input_;
-	int position_ = 0;
-	int readPosition_ = 0;
-	char ch_ = 0;
-
+    std::string_view input_;
+    int position_ = 0;
+    int readPosition_ = 0;
+    char ch_ = 0;
+    
 public:
-	Lexer(const std::string& input) : input_{input}
-	{
-		read_char();
-	}
-
-	void read_char() 
-	{
-		if (readPosition_ >= input_.size()) {
-			ch_ = 0;
-		}
-		else {
-			ch_ = input_[readPosition_];
-		}
-		position_ = readPosition_;
-		readPosition_ += 1;
-	}
-
-	Token next_token() 
-	{
-		Token token;
-
-		switch (ch_)
-		{
-		case '=':
-			token = { ASSIGN, input_.substr(position_, 1) };
-			break;
-		case ';':
-			token = { SEMICOLON, input_.substr(position_, 1) };
-			break;
-		case '(':
-			token = { LPAREN, input_.substr(position_, 1) };
-			break;
-		case ')':
-			token = { RPAREN, input_.substr(position_, 1) };
-			break;
-		case ',':
-			token = { COMMA, input_.substr(position_, 1) };
-			break;
-		case '+':
-			token = { PLUS, input_.substr(position_, 1) };
-			break;
-		case '{':
-			token = { LBRACE, input_.substr(position_, 1) };
-			break;
-		case '}':
-			token = { RBRACE, input_.substr(position_, 1) };
-			break;
-		case 0:
-			token = { EOF_, "" };
-			break;
-		default:
-			throw std::logic_error{ "next_token shouldn't come to default branch" };
-			break;
-		}
-		read_char();
-		return token;
-	}
+    Lexer(const std::string& input) : input_{input}
+    {
+        read_char();
+    }
+    
+    void read_char()
+    {
+        if (readPosition_ >= input_.size()) {
+            ch_ = 0;
+        }
+        else {
+            ch_ = input_[readPosition_];
+        }
+        position_ = readPosition_;
+        readPosition_ += 1;
+    }
+    
+    Token next_token()
+    {
+        Token token;
+        
+        switch (ch_)
+        {
+            case '=':
+                token = { ASSIGN, input_.substr(position_, 1) };
+                break;
+            case ';':
+                token = { SEMICOLON, input_.substr(position_, 1) };
+                break;
+            case '(':
+                token = { LPAREN, input_.substr(position_, 1) };
+                break;
+            case ')':
+                token = { RPAREN, input_.substr(position_, 1) };
+                break;
+            case ',':
+                token = { COMMA, input_.substr(position_, 1) };
+                break;
+            case '+':
+                token = { PLUS, input_.substr(position_, 1) };
+                break;
+            case '{':
+                token = { LBRACE, input_.substr(position_, 1) };
+                break;
+            case '}':
+                token = { RBRACE, input_.substr(position_, 1) };
+                break;
+            case 0:
+                token = { EOF_, "" };
+                break;
+            default:
+                if (is_letter(ch_)) {
+                    token.literal = read_identifier();
+                    token.type = lookup_ident(token.literal);
+                    return token;
+                } else {
+                    token = { ILLEGAL, input_.substr(position_, 1) };
+                }
+                break;
+        }
+        read_char();
+        return token;
+    }
+    
+    std::string_view read_identifier() {
+        auto position = position_;
+        while (is_letter(ch_)) {
+            read_char();
+        }
+        return input_.substr(position, position_ - position);
+    }
+    
+    TokenType lookup_ident(std::string_view ident) {
+        if (keywords.contains(ident)) {
+            return keywords[ident];
+        } else {
+            return IDENT;
+        }
+    }
 };
 
 }
